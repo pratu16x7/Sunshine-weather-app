f8679f57555af0baf8a5051987b8eef2ac14afa5
pratu16x7 <pratu16x7@gmail.com>
2016-06-21 17:55:43 +0530

    Add an intent to display detail

--- app/src/main/java/com/example/pratu16x7/sunshinefromscratch/ForecastFragment.java
+++ app/src/main/java/com/example/pratu16x7/sunshinefromscratch/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.pratu16x7.sunshinefromscratch;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -15,7 +16,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -96,9 +96,14 @@ public class ForecastFragment extends Fragment {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                CharSequence text = myForecastAdapter.getItem(position);
-                Toast toast = Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT);
-                toast.show();
+                String forecast = myForecastAdapter.getItem(position);
+                //Toast toast = Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT);
+                //toast.show();
+
+                Intent detailIntent = new Intent(getActivity(), DetailActivity.class);
+                detailIntent.putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(detailIntent);
+
             }
         });
 
