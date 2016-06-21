8d41198cb801fd2e0e884648d92148f48a5754e4
pratu16x7 <pratu16x7@gmail.com>
2016-06-21 17:26:06 +0530

    Create the new detail activity

---
+++ app/src/main/java/com/example/pratu16x7/sunshinefromscratch/DetailActivityFragment.java
@@ -0,0 +1,24 @@
+package com.example.pratu16x7.sunshinefromscratch;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DetailActivityFragment extends Fragment {
+
+    public DetailActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        return rootView;
+    }
+}
