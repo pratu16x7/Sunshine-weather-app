6d3e4eef7599806adfbef664f8e4880d7237f116
pratu16x7 <pratu16x7@gmail.com>
2016-06-29 03:50:02 +0530

    Implement update and delete, the all new FetchWeatherTask with addLocation and bulk inserts
    - addLocation was a a bit involved. The first time of actually using
    our WeatherProvider. Only thing to note, we use Content Resolvers to do
    that. TestProvider was a small guide.
    
    - Bulk inserts use transactions and were used to fill the database.
    Those values were then used to display on the UI

--- app/src/main/java/com/example/pratu16x7/sunshinefromscratch/data/WeatherProvider.java
+++ app/src/main/java/com/example/pratu16x7/sunshinefromscratch/data/WeatherProvider.java
@@ -259,17 +259,36 @@ public class WeatherProvider extends ContentProvider {
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         // Student: Start by getting a writable database
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
 
         // Student: Use the uriMatcher to match the WEATHER and LOCATION URI's we are going to
         // handle.  If it doesn't match these, throw an UnsupportedOperationException.
+        final int match = sUriMatcher.match(uri);
+        int numRows = -1;
 
         // Student: A null value deletes all rows.  In my implementation of this, I only notified
         // the uri listeners (using the content resolver) if the rowsDeleted != 0 or the selection
         // is null.
+        if (selection == null) selection = "1";
+        switch (match){
+            case WEATHER:
+                numRows = db.delete(WeatherContract.WeatherEntry.TABLE_NAME, selection, selectionArgs);
+                if (numRows == -1)
+                    throw new android.database.SQLException("Failed to delete rows from " + uri);
+                break;
+            case LOCATION:
+                numRows = db.delete(WeatherContract.LocationEntry.TABLE_NAME, selection, selectionArgs);
+                if (numRows == -1)
+                    throw new android.database.SQLException("Failed to delete rows from " + uri);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown URI: " + uri);
+        }
         // Oh, and you should notify the listeners here.
-
+        if (numRows > 0)
+            getContext().getContentResolver().notifyChange(uri, null);
         // Student: return the actual rows deleted
-        return 0;
+        return numRows;
     }
 
     private void normalizeDate(ContentValues values) {
@@ -285,7 +304,30 @@ public class WeatherProvider extends ContentProvider {
             Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         // Student: This is a lot like the delete function.  We return the number of rows impacted
         // by the update.
-        return 0;
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int numRows = -1;
+
+        switch (match){
+            case WEATHER:
+                normalizeDate(values);
+                numRows = db.update(WeatherContract.WeatherEntry.TABLE_NAME, values, selection, selectionArgs);
+                if (numRows == -1)
+                    throw new android.database.SQLException("Failed to delete rows from " + uri);
+                break;
+            case LOCATION:
+                numRows = db.update(WeatherContract.LocationEntry.TABLE_NAME, values, selection, selectionArgs);
+                if (numRows == -1)
+                    throw new android.database.SQLException("Failed to delete rows from " + uri);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown URI: " + uri);
+        }
+        // Oh, and you should notify the listeners here.
+        if (numRows > 0)
+            getContext().getContentResolver().notifyChange(uri, null);
+
+        return numRows;
     }
 
     @Override
